af3ae75aa7bf0ab1a79e0d2531e8b973e576d7ed
Remzi <remzitdf@abv.bg>
2021-02-15 09:55:55 +0200

    Add files via upload

---
+++ PalindromeIntegers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+                Console.WriteLine(isPalindrome(input));
+            }
+        }
+
+        private static string isPalindrome(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    return "false";
+                }
+            }
+            return "true";
+        }
+    }
+}
---
+++ PasswordValidation/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace PasswordValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            bool isValid = true;
+
+            if (!PassLength(password))
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+                isValid = false;
+            }
+            if (SymbolContain(password))
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+                isValid = false;
+            }
+            if (!DigitCount(password))
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+                isValid = false;
+            }
+            if (isValid)
+            {
+                Console.WriteLine("Password is valid");
+            }
+        }
+
+        static bool PassLength(string password)
+        {
+            return password.Length >= 6 && password.Length <= 10;
+        }
+
+        static bool SymbolContain(string password)
+        {
+
+            foreach (char symbol in password)
+            {
+                if (!char.IsLetterOrDigit(symbol))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static bool DigitCount(string password)
+        {
+            int count = 0;
+            foreach (char symbol in password)
+            {
+                if (char.IsDigit(symbol))
+                {
+                    count++;
+                }
+                if (count > 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
