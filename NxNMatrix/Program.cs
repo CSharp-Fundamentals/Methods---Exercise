f62290ca41e28fab68ed7535aa541b75af91fdd0
Remzi <remzitdf@abv.bg>
2021-02-15 09:55:11 +0200

    Add files via upload

---
+++ NxNMatrix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            PrintMatrix(size);
+        }
+
+        static void PrintMatrix(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(size+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
